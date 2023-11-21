aaaa6af30ef3073084a98b0714b2b572832cdd78
Tan Dang <tandang2002@live.dk>
2023-11-21 21:26:53 +0100

    Successfully added IO stream
    Can now read input file and change size variable according to the input files
    
    Attempt to make grass class (but not implemented yet)

--- src/Grass.java
+++ src/Grass.java
@@ -1,5 +1,49 @@
+import java.util.Random;
+import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
+import itumulator.world.Location;
 import itumulator.world.NonBlocking;
+import itumulator.world.World;
 
 public class Grass implements NonBlocking {
+    private boolean alive;
+    private Location location;
+    Random rand = new Random();
 
+    public Grass(Location location) {
+        this.location = location;
+        this.alive = true;
+    }
+
+    // Method to simulate grass decay
+    public void decay() {
+        // Implement logic for decaying
+        // For example, grass might have a chance to die
+        if (rand.nextDouble() < 0.1) { // 10% chance to decay
+            this.alive = false;
+        }
+    }
+
+    // Method to simulate grass spreading
+    public void spread(World world) {
+        // Implement logic for spreading
+        // For example, spread to a random adjacent location
+        if (this.alive && rand.nextDouble() < 0.2) { // 20% chance to spread
+            Set<Location> neighbours = world.getEmptySurroundingTiles();
+            List<Location> list = new ArrayList<>(neighbours);
+            Location newLocation = list.get(0); // Determine a new location to spread
+            world.setTile(newLocation, newLocation);
+        }
+    }
+
+    // Check if grass is alive
+    public boolean isAlive() {
+        return alive;
+    }
+
+    // Get the location of the grass
+    public Location getLocation() {
+        return location;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,46 +4,67 @@ import itumulator.world.Location;
 import itumulator.world.World;
 import java.awt.Color;
 import java.util.Random;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        int persons = 10; // The number of persons to simulate
-        Random r = new Random(); // Create a new random generator
-        int size = 5; // The size of the world (in tiles) (size x size)
-        int delay = 1000; // The delay between each simulation step (in ms)
-        int display_size = 800; // skærm opløsningen (i px)
-        Program p = new Program(size, display_size, delay); // Create a new program
-        World world = p.getWorld(); // Get the world from the program
+
+        int objects = 10; // Nummer af objekter der skal genereres
+        Random r = new Random(); // Laver en ny random generator
+        int size = 0; // Størrelsen af verdenen (i tiles) (dette er kvadratisk)
+        List<String> additionalLines = new ArrayList<>(); // Ny arraylist til at gemme de ekstra linjer i filen
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("./data/input-filer/t1-1a.txt")); // Læser input filen
+            String line = br.readLine();
+            if (line != null) { // På første linje sætter vi size til at være det første tal i filen
+                size = Integer.parseInt(line.split(" ")[0]);
+            }
+            while ((line = br.readLine()) != null) { // Læser derefter de andre linjer i filen og gemmer dem i en
+                                                     // arraylist
+                additionalLines.add(line);
+            }
+            br.close();
+        } catch (Exception e) {
+            System.out.println("File not found");
+        }
+
+        int delay = 1000; // Forsinkelsen mellem hver skridt af simulationen (i ms)
+        int display_size = 800; // Skærm opløsningen (i px)
+        Program p = new Program(size, display_size, delay); // Opret et nyt program
+        World world = p.getWorld(); // Hiv verdenen ud, som er der hvor vi skal tilføje ting!
 
         DisplayInformation di = new DisplayInformation(Color.red);
         p.setDisplayInformation(Person.class, di);
-        p.show(); // Show the simulation
+        p.show(); // Viser selve simulationen
 
         Person person = null; // Declare the person variable outside the loop
+        Grass grass = null;
 
-        // Create the persons
-        for (int i = 0; i < persons; i++) {
-            person = new Person(); // Create a new person
-            int x = r.nextInt(size); // Create a random x coordinate
-            int y = r.nextInt(size); // Create a random y coordinate
-            Location place = new Location(x, y); // Create a new location
-            while (!world.isTileEmpty(place)) { // While the tile is not empty
+        // Create the objects
+        for (int i = 0; i < objects; i++) {
+            person = new Person(); // Lav en ny person
+            int x = r.nextInt(size); // Lav et tilfældigt x koordinat
+            int y = r.nextInt(size); // Lav et tilfældigt y koordinat
+            Location place = new Location(x, y); // Lav ny lokation med de tilfældige koordinater
+            while (!world.isTileEmpty(place)) { // While tile ikke er tom
                 x = r.nextInt(size);
                 y = r.nextInt(size);
                 place = new Location(x, y);
             }
-            world.setTile(place, person); // Place the person on the location
+            world.setTile(place, person); // Placerer personen på den tilfældige lokation
         }
 
-        boolean isNight = true; // Declare and initialize the isNight variable
+        boolean isNight = true;
+        if (isNight) {
+            world.delete(person);
+        }
         // Runs 50 loops of the simulation
         for (int i = 0; i < 50; i++) {
             p.simulate();
-            if (isNight) {
-                world.delete(person);
-                world.step();
-            }
         }
     }
 }
