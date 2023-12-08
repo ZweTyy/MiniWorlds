ac256f639746844aa54dc69bb3711b88ae2d2aef
Tan Dang <tandang2002@live.dk>
2023-12-08 14:17:00 +0100

    added entityloader class

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,8 @@ import itumulator.executable.DisplayInformation;
 import itumulator.executable.Program;
 import itumulator.world.World;
 import java.awt.Color;
-import java.awt.geom.QuadCurve2D;
-import java.util.Random;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.util.Map;
-import java.util.Arrays;
-import java.util.LinkedHashMap;
 import itumulator.world.Location;
 
 public class Main {
@@ -42,57 +36,8 @@ public class Main {
         p.show();
         Rabbit.resetRabbitCount();
 
-        // Vi iterer igennem alle elementer der skal tilføjes
-        for (Map.Entry<String, Integer[]> entry : elementsToAdd.entrySet()) {
-            Location location;
-            String type = entry.getKey();
-            Integer[] quantityRange = entry.getValue();
-            int quantity = quantityRange[0];
-            for (int i = 0; i < quantity; i++) {
-                switch (type) {
-                    case ("rabbit"):
-                        Rabbit rabbit = RabbitFactory.createRabbit(world, size);
-                        location = rabbit.getLocation();
-                        placeEntity(world, rabbit, size);
-                        break;
-                    case ("grass"):
-                        Grass grass = GrassFactory.createGrass(world, size);
-                        location = grass.getLocation();
-                        placeEntity(world, grass, size);
-                        break;
-                    case ("burrow"):
-                        Burrow burrow = BurrowFactory.createBurrow(world, size);
-                        location = burrow.getLocation();
-                        placeEntity(world, burrow, size);
-                        break;
-                    case ("wolf"):
-                        Wolf wolf = WolfFactory.createWolf(world, size);
-                        location = wolf.getLocation();
-                        placeEntity(world, wolf, size);
-                        break;
-                    case ("bear"):
-                        Integer x = quantityRange.length > 2 ? quantityRange[1] : null;
-                        Integer y = quantityRange.length > 2 ? quantityRange[2] : null;
-                        Bear bear;
-                        if (x != null && y != null) {
-                            bear = BearFactory.createBear(world, size, x, y);
-                        } else {
-                            bear = BearFactory.createBear(world, size);
-                        }
-                        location = bear.getLocation();
-                        placeEntity(world, bear, size);
-                        break;
+        EntityLoader.loadEntities(world, elementsToAdd, size); // Tilføj alle elementer til verdenen
 
-                    case ("berry"):
-                        Berry berry = BerryFactory.createBerry(world, size);
-                        location = berry.getLocation();
-                        placeEntity(world, berry, size);
-                        break;
-                    default:
-                        break;
-                }
-            }
-        }
         int initialRabbitCount = countRabbits(world);
         System.out.println("Initial rabbit count: " + initialRabbitCount);
 
@@ -112,12 +57,5 @@ public class Main {
             }
         }
         return rabbitCount;
-    }
-
-    public static void placeEntity(World world, Entity entity, int size) {
-        Location location = entity.getLocation();
-        if (world.isTileEmpty(location)) {
-            world.setTile(location, entity);
-        }
     }
 }
