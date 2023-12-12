6fb06bfc820bf83fcb5b2b8a84887bd54f10a2b3
Tan Dang <tandang2002@live.dk>
2023-12-12 12:13:02 +0100

    wolf factory

--- src/WolfFactory.java
+++ src/WolfFactory.java
@@ -1,7 +1,13 @@
 import itumulator.world.World;
 
 public class WolfFactory {
-    public static Wolf createWolf(World world, int size) {
-        return new Wolf(world, size);
+    public static Wolf createWolf(World world, int size, Integer[] details) {
+        WolfPack wolfPack = new WolfPack(world, size);
+        for (int i = 0; i < details[0]; i++) {
+            Wolf wolf = new Wolf(world, size);
+            wolfPack.addMember(wolf);
+            wolf.setPack(wolfPack);
+        }
+        return wolfPack.getAlpha();
     }
 }
